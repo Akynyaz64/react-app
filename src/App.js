8e348a97aacdca98c576d49805626d021f1c1ee5
Akynyaz <akynyaz64@yandex.ru>
2022-01-05 20:56:09 +0500

    add css

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>Hello World!</p>
-        <ExpenseItem></ExpenseItem>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <p>Hello World!</p>
+                <ExpenseItem></ExpenseItem>
+            </header>
+        </div>
+    );
 }
 
 export default App;
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,15 @@
+import './ExpenseItem.css'
+
 function ExpenseItem () {
-    return <h2>ExpenseItem</h2>
+    return (
+        <div className="expense-item">
+            <div>Pizza Pepperoni</div>
+            <div className="expense-item__description">
+                <h2>25 min</h2>
+                <div className="expense-item__price">25$</div>
+            </div>
+        </div>
+    );
 }
 
 export default ExpenseItem;
